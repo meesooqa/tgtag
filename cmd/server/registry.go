2d3459005227993faf6d3f37b9f1e65f1b86c008
meesooqa <meesooqa@hotmail.com>
2025-02-27 18:41:11 +0300

    plug meesooqa/tgtag-ext-dummy #1

--- cmd/server/registry.go
+++ cmd/server/registry.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/meesooqa/tgtag-ext-dummy/ext/dummy_ext"
+
 	"github.com/meesooqa/tgtag/ext/main_ext"
 	"github.com/meesooqa/tgtag/pkg/extensions"
 	"github.com/meesooqa/tgtag/pkg/repositories"
@@ -8,5 +10,5 @@ import (
 
 func registerExtensions(repo repositories.Repository) {
 	extensions.Register(main_ext.NewMainExtension(repo))
-	//extensions.Register(dummy_ext.NewDummyExtension(repo))
+	extensions.Register(dummy_ext.NewDummyExtension(repo))
 }
