eef8f9f0bfb81caee6d4a76c18a6b211b7b36ffd
meesooqa <meesooqa@hotmail.com>
2025-02-28 10:03:15 +0300

    add coocc_ext #9

--- cmd/server/registry.go
+++ cmd/server/registry.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"github.com/meesooqa/tgtag-ext-dummy/ext/dummy_ext"
-
 	"github.com/meesooqa/tgtag/ext/main_ext"
 	"github.com/meesooqa/tgtag/pkg/extensions"
 	"github.com/meesooqa/tgtag/pkg/repositories"
+
+	"github.com/meesooqa/tgtag-ext-coocc/ext/coocc_ext"
+	"github.com/meesooqa/tgtag-ext-dummy/ext/dummy_ext"
 )
 
 func registerExtensions(repo repositories.Repository) {
 	extensions.Register(main_ext.NewMainExtension(repo))
 	extensions.Register(dummy_ext.NewDummyExtension(repo))
+	extensions.Register(coocc_ext.NewCooccExtension(repo))
 }
